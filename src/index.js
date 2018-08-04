b810ca47101aa9ede308eb728e0a5cfc991a4fd2
Luke Plaster <me@lukep.org>
2018-08-04 22:19:45 +0800

    Removed service worker that CRA added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
 import store from './store'
@@ -12,5 +11,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-registerServiceWorker();
